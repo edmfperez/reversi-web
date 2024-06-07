756dfacdeb2d5622c01a8c9867a2d11b6963fc3b
Eddie Perez <ed.perez@icloud.com>
2024-06-07 16:28:09 -0700

    amended server js to add chats

--- server.js
+++ server.js
@@ -23,13 +23,66 @@ function serverLog(...messages) {
 io.on('connection', (socket) => {
   serverLog('A page connected to the server:', socket.id);
 
+  socket.on('join_room', (payload) => {
+    serverLog('Server received join room command:', JSON.stringify(payload));
+
+    if (typeof payload === 'undefined' || payload === null) {
+      const response = {
+        result: 'fail',
+        message: 'Client did not send a payload'
+      };
+      socket.emit('join_room_response', response);
+      return;
+    }
+
+    const { room, username } = payload;
+    if (typeof room === 'undefined' || room === null) {
+      const response = {
+        result: 'fail',
+        message: 'Client did not send a valid room to join'
+      };
+      socket.emit('join_room_response', response);
+      return;
+    }
+
+    if (typeof username === 'undefined' || username === null) {
+      const response = {
+        result: 'fail',
+        message: 'Client did not send a valid username'
+      };
+      socket.emit('join_room_response', response);
+      return;
+    }
+
+    socket.join(room);
+    const sockets = Array.from(io.sockets.adapter.rooms.get(room) || []);
+    serverLog(`There are ${sockets.length} clients in the room ${room}`);
+    if (!sockets.includes(socket.id)) {
+      const response = {
+        result: 'fail',
+        message: 'Server internal error joining chat room'
+      };
+      socket.emit('join_room_response', response);
+      return;
+    }
+
+    const response = {
+      result: 'success',
+      room: room,
+      username: username,
+      count: sockets.length
+    };
+    io.in(room).emit('join_room_response', response);
+    serverLog('Join room succeeded:', JSON.stringify(response));
+  });
+
   socket.on('disconnect', () => {
     serverLog('A page disconnected from the server:', socket.id);
   });
 
-  // Handle receiving chat messages
+  // Handle chat messages
   socket.on('chat message', (data) => {
-    io.emit('chat message', data); // Broadcast the chat message to all clients
+    io.emit('chat message', data);
   });
 });
 
