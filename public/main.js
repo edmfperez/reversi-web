8e15f9f955c0de0f9201e47fd2089c5ec47071f6
Eddie Perez <ed.perez@icloud.com>
2024-06-07 16:46:35 -0700

    updated lobby and main js

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
+// main.js
 // Extract the username from the URL
 const params = new URLSearchParams(window.location.search);
-const username = decodeURI(params.get('username'))
-
+const username = decodeURI(params.get('username'));
 
 // Display the username in the lobby
 document.getElementById('username-display').innerText = `Welcome, ${username}`;
@@ -19,6 +19,18 @@ $(document).ready(() => {
   console.log('Client log message: Sending join room command', JSON.stringify(request));
   socket.emit('join_room', request);
 });
+
+function sendChatMessage() {
+  const message = $('#chat-input').val();
+  const request = {
+    room: chatRoom,
+    username: username,
+    message: message
+  };
+  console.log('Client log message: Sending chat message', JSON.stringify(request));
+  socket.emit('chat message', request);
+  $('#chat-input').val(''); // Clear the input field
+}
 
 // Handle server log messages
 socket.on('log', (message) => {
